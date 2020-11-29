768c73ee01343f6feeee10987c008eec7acd939a
Dametres <dametres.perry21@gmail.com>
2020-11-29 15:11:23 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
+class Artist
 
+    attr_accessor :song, :genre, :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @genre = genre
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name,genre)
+        Song.new(name,self,genre)
+    end
+
+    def songs 
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre
+        end
+    end
+end
